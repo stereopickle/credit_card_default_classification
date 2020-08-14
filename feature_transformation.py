3bd3dc844236fd6c6e747de8b2d098f3bbe8ced4
stereopickle <stereopickle@gmail.com>
2020-08-14 10:41:04 -0400

    removed 'Y'

--- feature_transformation.py
+++ feature_transformation.py
@@ -12,7 +12,7 @@ def feat_transform(df):
     df.columns = ['LIMIT_BAL', 'SEX', 'EDUCATION', 'MARRIAGE', 'AGE', 'PAY_0', 'PAY_2',
        'PAY_3', 'PAY_4', 'PAY_5', 'PAY_6', 'BILL_AMT1', 'BILL_AMT2',
        'BILL_AMT3', 'BILL_AMT4', 'BILL_AMT5', 'BILL_AMT6', 'PAY_AMT1',
-       'PAY_AMT2', 'PAY_AMT3', 'PAY_AMT4', 'PAY_AMT5', 'PAY_AMT6', 'Y']
+       'PAY_AMT2', 'PAY_AMT3', 'PAY_AMT4', 'PAY_AMT5', 'PAY_AMT6']
     
     pay_hist_names =[c for c in df if c.startswith('PAY')][0:6]
     bill_amt_names = [c for c in df if c.startswith('BILL_AMT')]
